de1fe62eeaef5b413efd189daf4578fc8b582557
drpriver <priver.david@gmail.com>
2020-12-01 23:11:28 -0500

    Add janky character roller

--- dd.js
+++ dd.js
@@ -1,4 +1,77 @@
 "use strict"
+
+function d6(){
+    return 1 + Math.floor(Math.random() * 6);
+}
+function roll_character(){
+    const hp = 7 + d6();
+    const gp = (d6()+d6()+d6())*10;
+    const abilities = [ "Strength", "Dexterity", "Agility", "Constitution", "Wisdom", "Intellect", "Charisma"];
+    const ability_scores = {};
+    for(let i = 0; i < abilities.length; i++){
+        ability_scores[abilities[i]] = d6()+d6()+d6();
+    }
+    const skills = [ "Alchemy", "Appraisal", "Climbing", "Cooking", "Decipher Script", "Disguise", "Escape Artist", "Forgery", "Herbalism", "Jumping", "Law", "Medicine", "Mining", "Navigation", "Pick Lock", "Pick Pocket", "Religion", "Riding", "Sailing", "Smithing", "Swimming", "Tracking" ];
+    const skill_scores = {};
+    for(let i = 0; i < skills.length; i++){
+        let score = d6() - 4;
+        if(score > 0){
+            skill_scores[skills[i]] = score;
+        }
+    }
+    const race_roll = d6();
+    var race;
+    if(race_roll <= 3){
+        race = "Human";
+    }
+    else if(race_roll == 4){
+        race = "Dwarf";
+    }
+    else if(race_roll == 5){
+        race = "Elf";
+    }
+    else {
+        const sub_roll = d6();
+        if(sub_roll <= 2){
+            race = "Hobbit";
+        }
+        else if(sub_roll == 3){
+            race = "Half-orc";
+        }
+        else {
+            race = "???";
+        }
+    }
+    function make_row(type, key, value){
+        var td1 = document.createElement(type);
+        td1.innerHTML = key;
+        var td2 = document.createElement(type);
+        td2.innerHTML = value;
+        var tr = document.createElement('tr');
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        return tr;
+    }
+    var output = document.createElement('table');
+
+    var firstrow = make_row('th', 'Attribute', 'Score');
+    output.appendChild(firstrow);
+    output.appendChild(make_row('td', 'Race', race));
+    output.appendChild(make_row('td', 'Gold', gp));
+    output.appendChild(make_row('td', 'HP', hp));
+    for(let i = 0; i < abilities.length; i++){
+        output.appendChild(make_row('td', abilities[i], ability_scores[abilities[i]]));
+    }
+    for(let i = 0; i < skills.length; i++){
+        let score = skill_scores[skills[i]];
+        if(score){
+            output.appendChild(make_row('td', skills[i], score));
+        }
+    }
+    const output_div = document.getElementById('rolled-character');
+    output_div.innerHTML = "";
+    output_div.appendChild(output);
+}
 function onload(){
     {
     const chapters = document.getElementsByClassName("chapter");
@@ -17,6 +90,8 @@ function onload(){
             el[0].classList.toggle("expanded");
         });
     }
+    const roll_button = document.getElementById("roll-button");
+    roll_button.onclick = roll_character;
 }
 document.addEventListener("DOMContentLoaded", onload)
 
